e7890648f967482298c383b7f1154ff76f5efc66
moltboie <moltboie@hoie.kim>
2026-02-14 04:08:20 -0800

    remove unnecessary @Global() decorator

--- apps/server/src/collaboration/collaboration.module.ts
+++ apps/server/src/collaboration/collaboration.module.ts
@@ -1,4 +1,4 @@
-import { Global, Logger, Module, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Logger, Module, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { AuthenticationExtension } from './extensions/authentication.extension';
 import { PersistenceExtension } from './extensions/persistence.extension';
 import { CollaborationGateway } from './collaboration.gateway';
@@ -12,7 +12,6 @@ import { LoggerExtension } from './extensions/logger.extension';
 import { CollaborationHandler } from './collaboration.handler';
 import { CollabHistoryService } from './services/collab-history.service';
 
-@Global()
 @Module({
   providers: [
     CollaborationGateway,
